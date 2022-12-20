a54418ac87bb2da81b0ceda0db9012461dd1c8a6
Awal Amoo <awalamoo7@gmail.com>
2022-12-20 03:33:46 +0000

    creating product details and post components

--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import queryString from "query-string";
 
 const Posts = ({ match, location }) => {
-  const result = queryString.parse(location.search);
-  console.log(result);
+	const result = queryString.parse(location.search);
+	console.log(result);
 
-  return (
-    <div>
-      <h1>Posts</h1>
-      Year: {match.params.year} , Month: {match.params.month}
-    </div>
-  );
+	return (
+		<div>
+			<h1>Posts Update</h1>
+			Year: {match.params.year} , Month: {match.params.month}
+		</div>
+	);
 };
 
 export default Posts;
